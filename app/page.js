bc3a99b45dcbc3f4de46c652e9cb512292e593a2
Abdayemco <wassim.abdayem@gmail.com>
2025-05-05 20:24:06 +0300

    Add files via upload

--- app/page.js
+++ app/page.js
@@ -1,8 +1,4 @@
-export default function HomePage() {
-  return (
-    <main>
-      <h1>Welcome to Abdayem Group</h1>
-      <p>This is the homepage.</p>
-    </main>
-  );
+
+export default function Home() {
+  return <h1>Welcome to Abdayem Group</h1>;
 }
